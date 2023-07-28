212243d1f2580f72237a41a6d4d016b42edf5e99
Berdishukur <boboqulovberdishukur@gmail.com>
2023-07-28 18:44:24 +0500

    create app

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR.joinpath('templates')],
+        'DIRS': [BASE_DIR.joinpath('dashboard/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,11 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "maxway_db",
-        'USER': "maxway_admin",
-        'PASSWORD': 'berdi1234',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'NAME': os.getenv("DB_NAME","maxway_db"),
+        'USER': os.getenv("DB_USER","maxway_admin"),
+        'PASSWORD': os.getenv("DB_PASSWORD","berdi1234"),
+        'HOST': os.getenv("DB_HOST","127.0.0.1"),
+        'PORT': os.getenv("DB_PORT","5432"),
+
     }
 }
 
