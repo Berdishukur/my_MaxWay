b2d2ce9dd34ce843f69ec03c890082005f02a8bf
Berdishukur <boboqulovberdishukur@gmail.com>
2023-08-02 17:48:16 +0500

    update product

--- food/models.py
+++ food/models.py
@@ -1,58 +1,48 @@
 from django.db import models
 
+
 class Category(models.Model):
-    title=models.CharField(max_length=100,blank=False,null=False)
-    created_at=models.DateTimeField(auto_now_add=True)
+    title = models.CharField(null=False, blank=False, max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
         return self.title
+
 
 class Product(models.Model):
-    title=models.CharField(max_length=100,blank=False,null=False)
-    descriptions=models.TextField(null=False,blank=False)
-    category=models.ForeignKey(Category,null=True,on_delete=models.SET_NULL)
-    cost=models.IntegerField(null=False,blank=False)
-    price=models.IntegerField(null=False,blank=False)
-    image=models.ImageField(upload_to='products')
-    created_at=models.DateTimeField(auto_now_add=True)
+    title = models.CharField(null=False, blank=False, max_length=100)
+    description = models.TextField(null=False, blank=False)
+    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    cost = models.IntegerField(null=False, blank=False)
+    price = models.IntegerField(null=False, blank=False)
+    image = models.ImageField(upload_to='products')
+    created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        return self.title
+        return f"{self.title}"
+
 
 class Customer(models.Model):
-    first_name=models.CharField(max_length=100,null=False,blank=False)
-    last_name=models.CharField(max_length=100,null=False,blank=False)
-    phone_nomer=models.CharField(max_length=100,unique=True,null=False,blank=False)
-    created_at=models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(null=False, blank=False, max_length=100)
+    last_name = models.CharField(null=False, blank=False, max_length=100)
+    phone_number = models.CharField(null=False, unique=True, blank=False, max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.first_name }  {self.last_name}"
+        return f"{self.first_name} {self.last_name}"
+
 
 class Order(models.Model):
-    payment_type=models.IntegerField(null=False,blank=False)
-    status=models.IntegerField(null=False,blank=True,default=1)
-    address=models.CharField(max_length=250,null=False,blank=False)
-    customer=models.ForeignKey(Customer,null=True,blank=True,on_delete=models.SET_NULL)
-    created_at=models.DateTimeField(auto_now_add=True)
+    payment_type = models.IntegerField(null=False, blank=False)
+    status = models.IntegerField(null=False, blank=True, default=1)
+    address = models.CharField(null=False, blank=False, max_length=250)
+    customer = models.ForeignKey(Customer, blank=True, null=True, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+
 
 class OrderProduct(models.Model):
-    count=models.IntegerField(null=False,blank=False)
-    price=models.IntegerField(null=False,blank=False)
-    created_at=models.DateTimeField(auto_now_add=True)
-    product=models.ForeignKey(Product,null=True,on_delete=models.SET_NULL)
-    order=models.ForeignKey(Order,null=True,on_delete=models.SET_NULL)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    count = models.IntegerField(null=False, blank=False)
+    price = models.IntegerField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
+    order = models.ForeignKey(Order, null=True, on_delete=models.SET_NULL)
