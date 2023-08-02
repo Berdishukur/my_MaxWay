baf3325aa5033a35cfdff1d0f5244ff0747b077d
Berdishukur <boboqulovberdishukur@gmail.com>
2023-08-02 11:58:46 +0500

    update product

--- dashboard/services.py
+++ dashboard/services.py
@@ -16,18 +16,35 @@ def dictfetchone(cursor):
     columns = [col[0] for col in cursor.description]
     return dict(zip(columns, row))
 
-def get_categories():
-    with closing(connection.cursor()) as cursor:
-        cursor.execute("""SELECT * from dashboard_category """)
-        category = dictfetchall(cursor)
-        return category
 
-def get_product():
+def get_order_by_user(id):
     with closing(connection.cursor()) as cursor:
-        cursor.execute(""" SELECT dashboard_product.id, dashboard_product.title, dashboard_product.descriptions, 
-        dashboard_product.price, 
-        dashboard_product.created, 
-        dashboard_category.name as category_name, dashboard_product.image as image  from dashboard_product
-        left join dashboard_category on dashboard_product.category_id = dashboard_category.id""")
-        product=dictfetchall(cursor)
-        return product
+        cursor.execute(""" SELECT food_order.id, food_customer.first_name,food_customer.last_name, food_order.address, food_order.payment_type,food_order.status,food_order.created_at from food_order 
+                            INNER JOIN food_customer on food_customer.id=food_order.customer_id 
+                            where food_order.customer_id =%s""", [id])
+        order = dictfetchall(cursor)
+        return order
+
+
+def get_product_by_order(id):
+    with closing(connection.cursor()) as cursor:
+        cursor.execute(""" SELECT food_orderproduct.count,food_orderproduct.price,
+        food_orderproduct.created_at,food_product.title from food_orderproduct 
+         INNER JOIN food_product ON food_orderproduct.product_id=food_product.id  where order_id=%s""", [id])
+        orderproduct = dictfetchall(cursor)
+        return orderproduct
+
+
+def get_table():
+    with closing(connection.cursor()) as cursor:
+        cursor.execute(""" 
+        SELECT food_orderproduct.product_id, 
+COUNT(food_orderproduct.product_id),food_product.title 
+FROM food_orderproduct 
+INNER JOIN food_product ON food_product.id=food_orderproduct.product_id 
+GROUP BY food_orderproduct.product_id ,food_product.title 
+order by count desc limit 10
+
+        """)
+        table = dictfetchall(cursor)
+        return table
