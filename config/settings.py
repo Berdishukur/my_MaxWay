0241d34441ac05cb0bf806f17832626910b8eddc
Berdishukur <boboqulovberdishukur@gmail.com>
2023-08-09 23:38:40 +0500

    update product

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'django.contrib.humanize',
-    # 'mathfilters',
+    'mathfilters',
 
     'dashboard',
     'food',
@@ -137,9 +137,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT =os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
