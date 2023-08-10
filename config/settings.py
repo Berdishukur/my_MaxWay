fd1de68518d4527083ccef455cb319d71462b599
Berdishukur <boboqulovberdishukur@gmail.com>
2023-08-10 20:47:30 +0500

    update product

--- config/settings.py
+++ config/settings.py
@@ -82,14 +82,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'maxway_db',
-        'USER':'maxway_user',
-        'PASSWORD':'berdi1234',
-        'HOST':'127.0.0.1',
-        'PORT':'5432',
+        'NAME': os.getenv('DB_NAME','maxway_db'),
+        'USER':os.getenv('DB_USER','maxway_user'),
+        'PASSWORD':os.getenv('DB_PASSWORD','berdi1234'),
+        'HOST':os.getenv('DB_HOST','127.0.0.1'),
+        'PORT':os.getenv('DB_PORT','5432'),
     }
 }
-#
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
@@ -134,7 +134,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
