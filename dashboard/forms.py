85a514a2a20b6840361c03b36c1c3883e48f1a97
Berdishukur <boboqulovberdishukur@gmail.com>
2024-01-09 20:07:00 +0500

    update product

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,14 +1,6 @@
 from django import forms
 from food.models import *
 
-class CategoryForm(forms.ModelForm):
-    class Meta:
-        model=Category
-        fields = "__all__"
-        widgets = {
-            "title": forms.TextInput(attrs={'class': 'form-control'}),
-
-        }
 
 class ProductForm(forms.ModelForm):
     class Meta:
